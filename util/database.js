ada4cd1e2f11228b6a71820dada369a4498b8ab9
gitBySharma <fetchsubhankar@gmail.com>
2025-02-06 22:09:48 +0530

    database connection update

--- util/database.js
+++ util/database.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize('charity-donation-platform', process.env.DB_USER, process.env.DB_PASSWORD, {
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     dialect: "mysql",
     host: process.env.DB_HOST
 });
